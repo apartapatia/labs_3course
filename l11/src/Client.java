f87e346bc1c455c305cd9258e3692099001565e0
apartapatia <crsonylove@gmail.com>
2023-09-05 10:28:06 +0300

    client and server created

--- l11/src/Client.java
+++ l11/src/Client.java
@@ -1,3 +1,83 @@
-public class Client {
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+public class Client {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage : java Client <server_host> <server_port>");
+            System.exit(1);
+        }
+
+        String serverHost = args[0];
+        int serverPort = Integer.parseInt(args[1]);
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Enter your name: ");
+            String initialName = userInput.readLine();
+
+            byte[] nameCommandData = ("@name" + initialName).getBytes();
+            InetAddress serverAddress = InetAddress.getByName(serverHost);
+            DatagramPacket nameCommandPacket = new DatagramPacket(nameCommandData, nameCommandData.length,
+                    serverAddress, serverPort);
+            socket.send(nameCommandPacket);
+
+            Thread receiveThread = new Thread(new MessageReceiver(socket));
+            receiveThread.start();
+
+            while (true) {
+                System.out.println("Enter a message (or type '@quit' to exit ");
+                String message = userInput.readLine();
+
+                if ("@quit".equals(message)) {
+                    System.out.println("Exiting chat.");
+                    break;
+                } else if (message.startsWith("@name")) {
+                    byte[] newNameCommandData = message.getBytes();
+                    DatagramPacket newNameCommandPacket = new DatagramPacket(newNameCommandData, newNameCommandData.length,
+                            serverAddress, serverPort);
+                    socket.send(newNameCommandPacket);
+                } else {
+                    byte[] sendData = message.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
+                            serverAddress, serverPort);
+                    socket.send(sendPacket);
+                }
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class MessageReceiver implements Runnable {
+        private DatagramSocket socket;
+
+        public MessageReceiver(DatagramSocket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    byte[] receiveData = new byte[1024];
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    socket.receive(receivePacket);
+
+                    String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- l11/src/Server.java
+++ l11/src/Server.java
@@ -1,2 +1,63 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.out.println("Usage : java Server <port>");
+            System.exit(1);
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        try {
+            DatagramSocket socket = new DatagramSocket(port);
+            System.out.println("Server is running on port " + port);
+
+            while (true){
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                        receiveData.length);
+                socket.receive(receivePacket);
+
+                String message = new String(receivePacket.getData(), 0,
+                        receivePacket.getLength());
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                Thread messageHandlerThread = new Thread(new MessageHandler(socket, message, clientAddress, clientPort));
+                messageHandlerThread.start();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    static class MessageHandler implements Runnable{
+        private DatagramSocket socket;
+        private String message;
+        private InetAddress clientAddress;
+        private int clientPort;
+
+        public MessageHandler(DatagramSocket socket, String message, InetAddress clientAddress, int clientPort){
+            this.socket = socket;
+            this.message = message;
+            this.clientPort = clientPort;
+            this.clientAddress = clientAddress;
+        }
+
+        @Override
+        public void run() {
+            if (message.startsWith("@name")){
+                String[] parts = message.split(" ");
+                if (parts.length >= 2){
+                    String newUserName = parts[1];
+                    System.out.println(clientAddress + ":" + clientPort + " - User set name to " + newUserName);
+                }
+            } else {
+                System.out.println(clientAddress + ":" + clientPort + " - " + message);
+            }
+        }
+    }
 }
