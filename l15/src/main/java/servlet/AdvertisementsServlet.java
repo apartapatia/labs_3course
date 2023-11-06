109fd29f2a4ec5afb6fd84b0619d4d30b97ec13f
apartapatia <crsonylove@gmail.com>
2023-11-06 14:46:23 +0300

    (main task) wait for additional task

--- l15/src/main/java/servlet/AdvertisementsServlet.java
+++ l15/src/main/java/servlet/AdvertisementsServlet.java
@@ -4,9 +4,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import model.AdvertisementsModel;
+import utils.DefaultServletHelper;
+import utils.ServletHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
 @WebServlet(urlPatterns = "/main")
 public class AdvertisementsServlet extends HttpServlet {
     private final List<AdvertisementsModel> advertisements = new ArrayList<>();
+    private final ServletHelper servletHelper = new DefaultServletHelper();
 
     @SneakyThrows
     @Override
@@ -28,9 +32,10 @@ public class AdvertisementsServlet extends HttpServlet {
     @SneakyThrows
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
         String username = (String) req.getSession().getAttribute("username");
 
-        if (username != null) {
+        if (servletHelper.isUserAuthenticated(session)) {
             String title = req.getParameter("title");
             String text = req.getParameter("text");
 
